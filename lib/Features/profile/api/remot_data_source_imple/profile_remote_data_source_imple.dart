ebaa9c68064b68deda38e75cdcaa6024a7f0982e
AhmedHussien249 <ahmed.hussien950.ah@gmail.com>
2026-02-10 06:47:05 +0200

    feat: Implement logout functionality within the profile module, including use case, data models, API, and repository integration.

--- lib/Features/profile/api/remot_data_source_imple/profile_remote_data_source_imple.dart
+++ lib/Features/profile/api/remot_data_source_imple/profile_remote_data_source_imple.dart
@@ -1,6 +1,7 @@
 import 'package:injectable/injectable.dart';
 import 'package:tracking_app/Features/profile/api/api_client/api_client.dart';
 import 'package:tracking_app/Features/profile/data/models/driver_profile_response.dart';
+import 'package:tracking_app/Features/profile/data/models/lib/Features/profile/data/models/logout_response.dart';
 import 'package:tracking_app/Features/profile/data/remote_data_source_contract/profile_remote_data_source_contract.dart';
 @Injectable(as: ProfileRemoteDataSourceContract)
 class ProfileRemoteDataSourceImple implements ProfileRemoteDataSourceContract {
@@ -12,5 +13,9 @@ class ProfileRemoteDataSourceImple implements ProfileRemoteDataSourceContract {
   @override
   Future<DriverProfileResponse> getDriverProfile() async {
     return await _api.getDriverProfile();
+  }
+  @override
+  Future<LogoutResponse> logout() async {
+    return await _api.logout();
   }
 }
