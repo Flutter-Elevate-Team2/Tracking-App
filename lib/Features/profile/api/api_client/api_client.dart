a8b499141ca09da96a9395792a66eba140cd2a5c
MoHa270xX <mhmdmedo102030@gmail.com>
2026-02-12 08:24:29 +0200

    feat: Add ProfileApi client for managing user profile features including profile retrieval, editing, password changes, and photo uploads.

--- lib/Features/profile/api/api_client/api_client.dart
+++ lib/Features/profile/api/api_client/api_client.dart
@@ -3,8 +3,13 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:tracking_app/Features/profile/data/models/change_password_request/change_password_request.dart';
 import 'package:tracking_app/Features/profile/data/models/driver_profile_response.dart';
+import 'package:tracking_app/Features/profile/data/models/edit_profile_request.dart';
+import 'package:tracking_app/Features/profile/data/models/edit_profile_response/change_password_response/change_password_response.dart';
+import 'package:tracking_app/Features/profile/data/models/edit_profile_response/edit_profile_response.dart';
 import 'package:tracking_app/Features/profile/data/models/logout_response.dart';
+import 'package:tracking_app/Features/profile/data/models/upload_photo/upload_photo_response.dart';
 import 'package:tracking_app/Features/profile/data/models/vehicles_response.dart';
 import 'package:tracking_app/core/constants/api_constants.dart';
 
@@ -32,4 +37,15 @@ abstract class ProfileApi {
 
   @GET(ApiConstants.vehicles)
   Future<VehiclesResponse> getVehicles();
+  @PUT(ApiConstants.editDriverProfile)
+  Future<EditProfileResponse> editProfile(@Body() EditProfileRequest request);
+  @PATCH(ApiConstants.changeDriverPassword)
+  Future<ChangePasswordResponse> changePassword(@Body() ChangePasswordRequest request);
+  @PUT(ApiConstants.uploadDriverPhoto)
+  @MultiPart() 
+  Future<UploadPhotoResponse> uploadPhoto(
+    @Part(name: "photo") File photo,
+     
+  );
+  
 }
