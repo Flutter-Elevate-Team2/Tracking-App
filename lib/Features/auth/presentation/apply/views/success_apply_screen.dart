9c0c21eef48e668fb1da10d69c6444996396e442
NaghamArafa <naghamarafa33@gmail.com>
2026-02-16 01:23:28 +0200

    refactor: enhance layout and structure of SuccessApplyScreen

--- lib/Features/auth/presentation/apply/views/success_apply_screen.dart
+++ lib/Features/auth/presentation/apply/views/success_apply_screen.dart
@@ -13,64 +13,69 @@ class SuccessApplyScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child:  Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-        Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30 , vertical: 50),
-        child:
-              Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 50,
+                ),
                 child: Lottie.asset(
                   repeat: false,
+                  height: MediaQuery.of(context).size.height * 0.35,
                   key: Key("successApplyLottie"),
                   Assets.lottie.checkAnimation,
                   fit: BoxFit.contain,
                 ),
-              ),),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        child:
-             Expanded(
+              ),
+            ),
 
-                 child:
-             Column(
-               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-               children: [
-                Text(
-                 key: Key("successApplySubTitle"),
-                 context.l10n.successApplySubTitle,
-                 style: Theme.of(context).textTheme.bodyLarge,
-                  textAlign: TextAlign.center,
-
-                ),
-               SizedBox(height: 25,),
-               Text(
-                 key: Key("successApplyDescription"),
-                 context.l10n.successApplyDescription,
-                 style: Theme.of(context).textTheme.bodyMedium,
-                 textAlign: TextAlign.center,
-               ),
-
-             ],)),
-      ),
-              Container(
-                padding: EdgeInsetsGeometry.symmetric(horizontal: 30 , vertical: 25),
-                key: Key("loginSuccessApply"),
-                child: CustomButton(
-                  title: context.l10n.loginTitle,
-                  onPressed: () {
-                    context.goNamed(Routes.loginName);
-                  },
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      key: Key("successApplySubTitle"),
+                      context.l10n.successApplySubTitle,
+                      style: Theme.of(context).textTheme.bodyLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 25),
+                    Text(
+                      key: Key("successApplyDescription"),
+                      context.l10n.successApplyDescription,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
               ),
-              Expanded(
-                child: Assets.images.bg.image(
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                  alignment: Alignment.bottomCenter,
-                ),
-                )
-            ],
+            ),
+            Container(
+              padding: EdgeInsetsGeometry.symmetric(
+                horizontal: 30,
+                vertical: 25,
+              ),
+              key: Key("loginSuccessApply"),
+              child: CustomButton(
+                title: context.l10n.loginTitle,
+                onPressed: () {
+                  context.goNamed(Routes.loginName);
+                },
+              ),
+            ),
+            Expanded(
+              child: Assets.images.bg.image(
+                width: double.infinity,
+                fit: BoxFit.cover,
+                alignment: Alignment.bottomCenter,
+              ),
+            ),
+          ],
         ),
       ),
     );
