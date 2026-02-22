6572219a8fbeee17cffc5146841caff59bd11d86
NaghamArafa <naghamarafa33@gmail.com>
2026-02-22 22:30:09 +0200

    feat: implemented the order feature including data, domain, and API layers to manage driver orders and status updates.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,71 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tracking_app/core/app_router/app_router.dart';
+import 'package:tracking_app/core/controller/session_controller.dart';
+import 'package:tracking_app/core/helpers/session_expired_handler.dart';
+import 'package:tracking_app/core/l10n/app_localizations.dart';
+import 'package:tracking_app/core/l10n/view_model/language_cubit.dart';
+import 'package:tracking_app/core/theming/app_theming.dart';
 
-void main() {
+import 'core/di/di.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  await configureDependencies();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _sessionController = getIt<SessionController>();
+  late StreamSubscription? _subscription;
+  @override
+  void initState() {
+    super.initState();
+    _subscription = _sessionController.onSessionExpired.listen((_) {
+      // Fix: Check mounted and pass correct context
+      final context = AppRouter.rootNavigatorKey.currentContext;
+      if (context != null && mounted) {
+        SessionExpiredHandler.handle(context);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // Fix: Safe cancel
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => LanguageCubit())],
+      child: BlocBuilder<LanguageCubit, Locale>(
+        builder: (context, locale) {
+          return MaterialApp.router(
+            locale: locale,
+            routerConfig: AppRouter.router,
+            debugShowCheckedModeBanner: false,
+            supportedLocales: AppLocalizations.supportedLocales,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            theme: AppTheme.lightTheme,
+          );
+        },
+      ),
+    );
   }
 }
